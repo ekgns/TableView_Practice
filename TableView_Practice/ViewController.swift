1e813538fb577fad8f541b6ef86802c8731ae8af
ekgns14 <ekgns14@naver.com>
2021-02-06 00:55:17 +0900

    Initial Commit

---
+++ TableView_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView_Practice
+//
+//  Created by 다훈김 on 2021/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
